7aaa782e98c1fb2921db743c937a968558463594
Dallin <dallin@Dallins-MacBook-Pro-3.local>
2022-09-30 13:51:53 -0600

    added connecton to redis

--- server.js
+++ server.js
@@ -3,12 +3,21 @@ const bodyParser = require("body-parser");
 const {v4 : uuidv4} = require("uuid");
 const port = 3000;
 const app = express();
+const md5 = require('md5');
+const {createClient} = require('redis');
+const { fstat } = require('fs');
+const redisClient = createClient(
+{
+    Url:'redis://default@localhost:6379',
+}
+);
 
 app.use(bodyParser.json());
 
 
 app.listen(port, async ()=>{
-    console.log('Listening on port '+port);
+    await redisClient.connect();
+    console.log('Listening on port ',port);
 });
 
 
@@ -17,5 +26,29 @@ app.get("/", (req,res)=>{
 });
 
 app.post("/login", (req,res)=>{
-    
+
+});
+
+app.post('/user',(req,res)=>{
+    const newUserRequestObject = eq.body;
+    comsole.log('New User:', JSON.stringify(newUserRequestObject));
+    redisClient.hSet('users',req.body.email,JSON.stringify(newUserRequestObject));
+    res.send("New user added"+newUserRequestObject.email+" added");    
+});
+
+app.post('/login', (req,res)=>{
+    const loginEmail = req.body.userName;
+    console.log(JSON.stringify(req.body));
+    console.log("loginEmail", loginEmail);
+    const loginPassword = req.body.password;
+    console.log("loginPassword", loginpassword);
+    // res.sendStatus("Who are you"); //
+
+if (loginEmail == "gary@test.com" && loginPassword == "Bruh#1"){
+        const token = uuidv4();
+        res.send(token);
+    } else{
+        res.status(401);//unauthorized
+        res.send("Invalid user or password");
+    }
 })
--- signin.sh
+++ signin.sh
@@ -1,4 +1,5 @@
 signin.sh
     curl -v -d "@signin.json" -X POST -H "Content-Type:application/json" http://localhost:3000/login
 
-    # htpps://dev.stedi.me/login
+    # https://dev.stedi.me/login
+    # http://localhost:3000/login
