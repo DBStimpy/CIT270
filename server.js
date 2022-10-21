b24a104888916238271f295415616e5c32bf8863
Dallin <dallin@Dallins-MacBook-Pro-3.local>
2022-10-21 13:44:24 -0600

    added connecton to redis

--- server.js
+++ server.js
@@ -55,3 +55,15 @@ else if (loginEmail == userObject.userName && loginPassword == userObject.passwo
         res.send("Invalid user or password");
     }
 })
+
+https.createServer({
+
+    key: fs.readFileSync('server.key'),
+    cert: fs.readFileSync('server.cert'),
+    ca: fs.readFileSync('chain.pem'),
+    // passphrase: 'P@ssw0rd'
+
+}, app).listen(port, async () => {
+    await redisClient.connect();
+    console.log('Listening...')
+});
